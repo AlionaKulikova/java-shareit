8e6044971f483a8d7487b61f75f30af406713e06
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2025-03-03 22:08:57 +0300

    исправилп замечания

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -8,43 +8,13 @@ import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-/**
- * Класс, представляющий запрос на предмет.
- *
- * <p>Этот класс используется для хранения информации о запросе на предмет,
- * включая уникальный идентификатор, описание, пользователя, создавшего запрос,
- * и дату создания запроса.</p>
- */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemRequest {
-    /**
-     * Уникальный идентификатор запроса.
-     *
-     * @return уникальный идентификатор запроса
-     */
     private Long id;
-
-    /**
-     * Текст запроса, содержащий описание требуемой вещи.
-     *
-     * @return описание запрашиваемой вещи
-     */
     private String description;
-
-    /**
-     * Пользователь, создавший запрос.
-     *
-     * @return пользователь, создавший запрос
-     */
     private User requestor;
-
-    /**
-     * Дата и время создания запроса.
-     *
-     * @return дата и время создания запроса
-     */
     private LocalDateTime created;
 }
